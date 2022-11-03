a632118f50625d71df2a2dc08e68ed5f318a951a
LittlSkittl <Isaacmalin3530@gmail.com>
2022-11-03 12:22:51 -0600

    Initial commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import "./NavBar.scss"
+
+const NavBar = () => {
+    return (
+        <div className="Nav-Container">
+            <nav className="Nav-Bar">
+                <p>About Me</p>
+                <p>My Work</p>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
