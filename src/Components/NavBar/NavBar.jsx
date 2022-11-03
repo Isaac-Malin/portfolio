d2c3ce4f569c461e0087ff358840422f11d31a35
LittlSkittl <Isaacmalin3530@gmail.com>
2022-11-03 16:48:06 -0600

    Added a few styles to the Nav bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,6 +3,9 @@ import "./NavBar.scss"
 const NavBar = () => {
     return (
         <div className="Nav-Container">
+            <div className="My-Name">
+                <p>Isaac Malin</p>
+            </div>
             <nav className="Nav-Bar">
                 <p>About Me</p>
                 <p>My Work</p>
