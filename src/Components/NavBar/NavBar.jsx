c15c4912a64099af1a0cbdc1a4380e2cbaf21029
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-11-04 01:20:33 -0600

    Unfinished Nav Bar Styling

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,11 +4,11 @@ const NavBar = () => {
     return (
         <div className="Nav-Container">
             <div className="My-Name">
-                <p>Isaac Malin</p>
+                <h1 className="links">Isaac Malin</h1>
             </div>
             <nav className="Nav-Bar">
-                <p>About Me</p>
-                <p>My Work</p>
+                <button className="links1">About Me</button>
+                <button className="links1">My Work</button>
             </nav>
         </div>
     )
